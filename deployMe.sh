701d8a48d215890a10660978ee1f9308f6450b37
alex <alex.maties@lextech.com>
2015-11-10 11:50:02 -0600

    Updated the deployment script and jetty realm

--- deployMe.sh
+++ deployMe.sh
@@ -1,50 +1,36 @@
-## ---------------------------------------------------------------------------
-## Licensed to the Apache Software Foundation (ASF) under one or more
-## contributor license agreements.  See the NOTICE file distributed with
-## this work for additional information regarding copyright ownership.
-## The ASF licenses this file to You under the Apache License, Version 2.0
-## (the "License"); you may not use this file except in compliance with
-## the License.  You may obtain a copy of the License at
-##
-## http://www.apache.org/licenses/LICENSE-2.0
-##
-## Unless required by applicable law or agreed to in writing, software
-## distributed under the License is distributed on an "AS IS" BASIS,
-## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-## See the License for the specific language governing permissions and
-## limitations under the License.
-## ---------------------------------------------------------------------------
-
-# Defines users that can access the web (console, demo, etc.)
-# username: password [,rolename ...]
-admin: admin, admin
-user: user, user
 #!/bin/bash -v
-echo Stop sendmail... > /home/ec2-user/activemqSetup.log 2>&1
-/etc/init.d/sendmail stop > /home/ec2-user/activemqSetup.log 2>&1
+# yum update -y
+# yum install -y java-1.8.0-openjdk.x86_64
+# yum erase -y java-1.7.0-openjdk
+# yum install -y git
+# yum install -y tomcat7
+# yum install git -y
 
-echo Updating system... > /home/ec2-user/activemqSetup.log 2>&1
-yum -y update >> /home/ec2-user/activemqSetup.log 2>&1
 
-echo "Creating folder for ActiveMQ..." >> /home/ec2-user/activemqSetup.log 2>&1
-mkdir /opt/activemq >> /home/ec2-user/activemqSetup.log 2>&1
-cd /opt/activemq/ >> /home/ec2-user/activemqSetup.log 2>&1
+cd /tmp
+git clone https://bitbucket.org/DigitalMfgCommons/dmcactivemq.git
+cd dmcactivemq
+mv * ..
 
-echo "Downloading and unpacking dependencies..." >> /home/ec2-user/activemqSetup.log 2>&1
-yum install -y wget >> /home/ec2-user/activemqSetup.log 2>&1
-wget http://archive.apache.org/dist/activemq/apache-activemq/5.9.0/apache-activemq-5.9.0-bin.tar.gz >> /home/ec2-user/activemqSetup.log 2>&1
-tar -zxvf apache-activemq-5.9.0-bin.tar.gz >> /home/ec2-user/activemqSetup.log 2>&1
+/etc/init.d/sendmail stop 
 
-echo "Creating ActiveMQ user..." >> /home/ec2-user/activemqSetup.log 2>&1
-/usr/sbin/useradd activemq >> /home/ec2-user/activemqSetup.log 2>&1
-touch /etc/default/activemq >> /home/ec2-user/activemqSetup.log 2>&1
-chown activemq /etc/default/activemq >> /home/ec2-user/activemqSetup.log 2>&1
-chmod 600 /etc/default/activemq >> /home/ec2-user/activemqSetup.log 2>&1
+mkdir /opt/activemq 
+cd /opt/activemq/ 
 
-echo "Starting ActiveMQ..." >> /home/ec2-user/activemqSetup.log 2>&1
-sudo su activemq >> /home/ec2-user/activemqSetup.log 2>&1
+wget http://archive.apache.org/dist/activemq/apache-activemq/5.9.0/apache-activemq-5.9.0-bin.tar.gz 
+tar -zxvf apache-activemq-5.9.0-bin.tar.gz 
+/usr/sbin/useradd activemq 
+touch /etc/default/activemq 
+chown activemq /etc/default/activemq 
+chmod 600 /etc/default/activemq 
+sudo su activemq 
+
+cd /tmp
+echo "admin : $activeMqRoot, $activeMqRootPass" >> jetty-realm.properties 
+echo "user : $activeUser, $activeMqUserPass" >> jetty-realm.properties 
 
 cp -v  /tmp/jetty-realm.properties /opt/activemq/apache-activemq-5.9.0/conf/jetty-realm.properties
-chmod 644 /opt/activemq/apache-activemq-5.9.0/conf/jetty-realm.properties
 
-/opt/activemq/apache-activemq-5.9.0/bin/activemq start >> /home/ec2-user/activemqSetup.log 2>&1
+
+chmod 644 /opt/activemq/apache-activemq-5.9.0/conf/jetty-realm.properties
+/opt/activemq/apache-activemq-5.9.0/bin/activemq start 
