fb885e784d4119efc8d968488ae5423d528aa502
Cyprien Mvuanda <cyprien.mvuanda@turner.com>
2015-11-11 18:22:47 -0500

    Configuring activemq service to start automatically

--- deployMe.sh
+++ deployMe.sh
@@ -31,6 +31,16 @@ echo "user : $activeUser, $activeMqUserPass" >> jetty-realm.properties
 
 cp -v  /tmp/jetty-realm.properties /opt/activemq/apache-activemq-5.9.0/conf/jetty-realm.properties
 
+# Copy our custom startup script to /etc/init.d and set appropriate permissions
+# this makes the command "service activemq start|stop|restart" possible
+cp /tmp/activemq /etc/init.d/.
+chmod 755 /etc/init.d/activemq
+# Configure sytem to start the activemq service automatically
+chkconfig activemq on
+
+
+
+
 
 chmod 644 /opt/activemq/apache-activemq-5.9.0/conf/jetty-realm.properties
 /opt/activemq/apache-activemq-5.9.0/bin/activemq start 
